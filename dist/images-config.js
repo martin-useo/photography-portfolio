30650041c1e4ac179283e0294bff0526eb2863df
Martin <martin.useo@gmail.com>
2025-11-10 23:44:14 +0100

    Local images

---
+++ dist/images-config.js
@@ -0,0 +1,44 @@
+const portfolioImages = [
+  {
+    id: 1,
+    src: '/dist/assets/images/ThéoNoJitensha-1.jpg',
+    alt: 'a bike',
+    size: 'md:w-1/2'
+  },
+  {
+    id: 2,
+    src: '/dist/assets/images/ThéoNoJitensha-2.jpg',
+    alt: 'another bike',
+    size: 'md:w-1/2'
+  },
+  {
+    id: 3,
+    src: '/dist/assets/images/ThéoNoJitensha-3.jpg',
+    alt: 'another another bike',
+    size: 'w-full'
+  },
+  {
+    id: 4,
+    src: '/dist/assets/images/ThéoNoJitensha-4.jpg',
+    alt: 'still a bike',
+    size: 'w-full'
+  },
+  {
+    id: 5,
+    src: '/dist/assets/images/ThéoNoJitensha-5.jpg',
+    alt: 'yet another bike',
+    size: 'md:w-1/2'
+  },
+  {
+    id: 6,
+    src: '/dist/assets/images/ThéoNoJitensha-6.jpg',
+    alt: 'bikey bikey',
+    size: 'md:w-1/2'
+  },
+  {
+    id: 7,
+    src: '/dist/assets/images/ThéoNoJitensha-7.jpg',
+    alt: 'two wheels bike',
+    size: 'md:w-1/2'
+  }
+];
